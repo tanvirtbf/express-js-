dc22bd9f9f0472b5e92eb11a20335452d727c8c2
tanvirtbf <tanver018765@gmail.com>
2024-10-05 15:17:50 +0600

    babel definition note

---
+++ cluster0/Setup Babel with Express JS/note.js
@@ -0,0 +1,12 @@
+// What is Babel ? 
+
+// Babel is a toolchain that is mainly used to convert ECMAScript 2015+ code into a backwards compatible version of Javascript in current and older browsers or environments. Here are the main things Babel can do for you.
+
+// r ki ki korte pare Babel ? 
+// 1. Transform Syntax 
+// 2. polyfill features that are missing in your target environment
+// 3. Source code transformations
+
+// note : browser akhno update hoy nai je se latest code bujhbe . browser akhno old javascript code bujhe . kintu developer ra to kaj easy korar jonno latest javascript er code kore . tai Babel sei latest code ke browser jei old code bujhe sei code e convert kore 
+
+
