047357dfa60fad7552c8e3609a9b84497f5cc69d
tanvirtbf <tanver018765@gmail.com>
2024-10-05 15:26:25 +0600

    update

--- cluster0/Setup Babel with Express JS/note.js
+++ cluster0/Setup Babel with Express JS/note.js
@@ -9,4 +9,20 @@
 
 // note : browser akhno update hoy nai je se latest code bujhbe . browser akhno old javascript code bujhe . kintu developer ra to kaj easy korar jonno latest javascript er code kore . tai Babel sei latest code ke browser jei old code bujhe sei code e convert kore 
 
+// Express js er moddhe Babel ke setup korar jonno kon kon package use korte hobe?
 
+// 1. @babel/core
+// 2. @babel/cli
+// 3. @babel/preset-env
+
+// 1. @babel/core 
+// This is the main package to run any babel setup or configuration
+
+// 2. @babel/cli 
+// Babel comes with a built in CLI which can be used to compile files from the command line (ai package taii muloto latest code ke old code e convert kore)
+
+// 3. @babel/preset-env
+// This enables us to use new and upcoming features which node.js is yet to understand. New features are always new and will probably take time to implement in NodeJS default. (jehetu express js internally node js e convert hoy tai jodi amon kono latest feature ashe jeta node js e akhno implement hoy nai sei latest code ke competible node js code e convert kore ai package)
+
+// How to install that 3 package ? 
+// npm install @babel/core @babel/cli @babel/preset-env
