aea5f14222d061685425b2dff975fd83286ae97c
tanvirtbf <tanver018765@gmail.com>
2024-10-06 22:34:32 +0600

    regular expression path

--- cluster0/Routing/app.js
+++ cluster0/Routing/app.js
@@ -17,18 +17,21 @@ const port = process.env.PORT || '3000'
 //   res.send('API Page')
 // }) // (/api er por sob route er jonno with any method...)
 
-// Pattern Pattern Path 
-app.get('/ab?cd', (req,res)=>{
-  res.send('This route path will match acd and abcd')
-}) // This route path will match acd and abcd
+// String Pattern Path 
+// app.get('/ab?cd', (req,res)=>{
+//   res.send('This route path will match acd and abcd')
+// }) // This route path will match acd and abcd
 // only /acd or /abcd route e hit hobe
 
-
+// Regular Expression Path
+// app.get(/a/, (req,res)=>{
+//   res.send('Find a')
+// }) // jekono route jekhane a symbol ache setateii ai callback hit hobe . jemon /fasd -- hit , /asdfsadf/fdssdf/dfd -- hit etc..
 
 
 app.listen(port, ()=>{
   console.log(`Server start with http://localhost:${port}`)
-})
+}) 
 
 
 
