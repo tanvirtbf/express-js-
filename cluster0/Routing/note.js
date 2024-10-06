e00535025f3bdc3596778dad91559de5292d3e7d
tanvirtbf <tanver018765@gmail.com>
2024-10-06 12:04:16 +0600

    update

--- cluster0/Routing/note.js
+++ cluster0/Routing/note.js
@@ -7,11 +7,28 @@
 // Syntax : 
 // app.method(path, callback)
 
-// method --- GET ? POST ? PUT ? PATCH ? DELETE ?
+// app ---- app is a instance of express
+// method --- GET ? POST ? PUT ? PATCH ? DELETE ? method is an HTTP request method in lowercase
 // path --- which url have search or send request by user
 // callback --- ai path e user hit korle ki response pabe setaii ekhane deya 
 
 // Another Syntax : 
-// app.method(path, [callback1, callback2, callback3,....])
+// app.method(path, [callback1, callback2, callback3,....]) 
+// OR 
+// app.method(path, [callback1, callback2,...], callback)
 // jodi 1 er beshi callback run koraite chai ai url e hit korle tahole syntax aita. 
+
+// Example : 
+// app.get('/', (req,res)=>{...})
+// app.post('/signup', (req,res)=>{...})
+
+// Methods 
+// GET --- Retrieve Data 
+// POST --- Create/Insert Data
+// PUT --- Completely Update Data
+// PATCH --- Partially Update Data
+// DELETE --- Delete Data
+// ALL --- Any HTTP Request Method (eta express nije banaise . jodi ami sure na thaki kon method e request ashce tokhon ai method use hoy)
+
+
 
