bd38da0e2b868b76c71a3509fb1e29caaa663f78
tanvirtbf <tanver018765@gmail.com>
2024-10-06 22:30:12 +0600

    pattern path

--- cluster0/Routing/app.js
+++ cluster0/Routing/app.js
@@ -1,7 +1,7 @@
 import express from 'express'
 const app = express()
 
-const port = process.env.PORT || '3001'
+const port = process.env.PORT || '3000'
 
 // app.all('/', (req,res)=>{
 //   res.send('Hello Post!')
@@ -18,7 +18,10 @@ const port = process.env.PORT || '3001'
 // }) // (/api er por sob route er jonno with any method...)
 
 // Pattern Pattern Path 
-
+app.get('/ab?cd', (req,res)=>{
+  res.send('This route path will match acd and abcd')
+}) // This route path will match acd and abcd
+// only /acd or /abcd route e hit hobe
 
 
 
