da299130a882a112d34a348f5999d6a02668c9f9
tanvirtbf <tanver018765@gmail.com>
2024-10-06 10:30:21 +0600

    express generator create note

---
+++ cluster0/Express Application Generator Tool/note.js
@@ -0,0 +1,17 @@
+// Express Application Generator
+
+// Use the application generator tool, express-generator, to quickly create an application skeleton. 
+
+// 1st approach 
+// npx express-generator --view=ejs myapp
+
+
+// 2nd approach 
+// npm install -g express-generator (express generator ke globally install korlam)
+// express -view=ejs myapp 
+// then npm install kore sob dependencies install korte hobe
+// npm install
+// then command prompt e ai command run korte hobe
+// set DEBUG=myapp:* & npm start (myapp holo amar project er nam)
+
+
