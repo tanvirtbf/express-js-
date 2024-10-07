fe98f93220e1ffe6a090d00ceac1c043403f7aed
tanvirtbf <tanver018765@gmail.com>
2024-10-07 11:43:45 +0600

    dynamic routing and params

--- cluster0/Route Parameter and Query String/app.js
+++ cluster0/Route Parameter and Query String/app.js
@@ -4,9 +4,30 @@ const app = express()
 
 const port = process.env.PORT || '3000'
 
-app.post('/',(req,res)=>{
-  res.send('Hello')
+// app.get('/student/delete/1',(req,res)=>{
+//   res.send('Student Deleted!')
+// })
+// app.get('/student/delete/2',(req,res)=>{
+//   res.send('Student Deleted!')
+// })
+// app.get('/student/delete/3',(req,res)=>{
+//   res.send('Student Deleted!')
+// })
+
+// Dynamic Routing 
+// app.get('/student/delete/:id',(req,res)=>{
+//   res.send('Student Deleted!')
+// })
+
+// Params 
+app.get('/student/delete/:id',(req,res)=>{
+  const {id} = req.params
+  console.log(req.params)
+  console.log(req.params.id)
+  console.log(id)
+  res.send(`Student Deleted! ${id}`)
 })
+
 
 app.listen(port, ()=>{
   console.log(`App Run with http://localhost:${port}`)
