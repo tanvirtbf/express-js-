a24ce074a1680ad4b26e79de9f93bd8594a7e081
tanvirtbf <tanver018765@gmail.com>
2024-10-07 12:13:00 +0600

    Route Parameter

--- cluster0/Route Parameter and Query String/app.js
+++ cluster0/Route Parameter and Query String/app.js
@@ -20,13 +20,40 @@ const port = process.env.PORT || '3000'
 // })
 
 // Params 
-app.get('/student/delete/:id',(req,res)=>{
-  const {id} = req.params
-  console.log(req.params)
-  console.log(req.params.id)
-  console.log(id)
-  res.send(`Student Deleted! ${id}`)
-})
+// app.get('/student/delete/:id',(req,res)=>{
+//   const {id} = req.params
+//   console.log(req.params)
+//   console.log(req.params.id)
+//   console.log(id)
+//   res.send(`Student Deleted! ${id}`)
+// })
+
+// Another Example : 
+// app.get('/product/:category/:id',(req,res)=>{
+//   const {category,id} = req.params
+//   console.log(req.params)
+//   console.log(req.params.category)
+//   console.log(req.params.id)
+//   res.send(`Product Category : ${req.params.category} <br /> Product Id : ${req.params.id}`)
+// })
+
+// Another Example : 
+// app.get('/product/:category/and/:id', (req,res)=>{
+//   const {category,id} = req.params 
+//   res.send(`Category : ${category} and Id : ${id}`)
+// })
+
+// Another Example : 
+// app.get('/train/:from-:to',(req,res)=>{
+//   const {from,to} = req.params
+//   res.send(`From : ${from} and To : ${to}`)
+// })
+
+// Another Example : 
+// app.get('/train/:from.:to',(req,res)=>{
+//   const {from,to} = req.params
+//   res.send(`From : ${from} and To : ${to}`)
+// })
 
 
 app.listen(port, ()=>{
